ea7fcba7f1768e5f1d0f1bdeb9bcc28e2f647e4c
jesus <jesusalvan2010@hotmail.com>
2020-07-29 20:31:50 -0500

    Adding Google and Facebook auth

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,25 +1,33 @@
-import React, {Fragment} from 'react';
-import { Layout } from 'antd';
+import React, { Fragment } from "react";
+import { Layout } from "antd";
+import { useRouter } from "next/router";
 
-import Head from './Head/HeadLayout';
-import HeaderLayout from './Header/HeaderLayout';
-import ContentLayout from './Content/ContentLayout';
-import FooterLayout from './Footer/FooterLayout';
-import SubHeaderLayout from '../layouts/SubHeader/SubHeaderLayout';
+import Head from "./Head/HeadLayout";
+import HeaderLayout from "./Header/HeaderLayout";
+import ContentLayout from "./Content/ContentLayout";
+import FooterLayout from "./Footer/FooterLayout";
+import SubHeaderLayout from "../layouts/SubHeader/SubHeaderLayout";
 
-const MainLayout = ({ children}) => {
-    return (
-        <Fragment>
-            <Head />
-            <Layout className="layout">
-                <HeaderLayout />
-                <SubHeaderLayout /> 
-                <ContentLayout children={children} />
-                <FooterLayout />
-            </Layout>
-        </Fragment>
+import { SignContainer } from "./layoutsStyledComponents"; 
 
-    );
-}
- 
+const MainLayout = ({ children }) => {
+  const router = useRouter();
+  return (
+    <Fragment>
+      <Head />
+
+      {router.pathname === "/sign-in" || router.pathname === "/sign-up" ? (
+          <SignContainer>{children}</SignContainer>
+      ) : (
+        <Layout className="layout">
+          <HeaderLayout />
+          <SubHeaderLayout />
+          <ContentLayout children={children} />
+          <FooterLayout />
+        </Layout>
+      )}
+    </Fragment>
+  );
+};
+
 export default MainLayout;
---
+++ components/layouts/layoutsStyledComponents.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const SignContainer = styled.div`
+background-color: black;
+color: white;
+height: 100%;
+width: 100%;
+`;
